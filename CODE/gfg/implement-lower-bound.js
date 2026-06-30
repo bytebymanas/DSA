752327b319b14b74e7e45321ec8db4df897e88c8
Manas Chhabra <163338402+bytebymanas@users.noreply.github.com>
2026-06-30 13:29:44 +0530

    Add gfg:Implement Lower Bound

--- CODE/gfg/implement-lower-bound.js
+++ CODE/gfg/implement-lower-bound.js
@@ -10,8 +10,29 @@
 
 class Solution {
   public:
-    int lowerBound(vector<int>& arr, int target) {
-        // code here
+    int lowerBound(vector<int>& nums, int target) {
+        int st = 0 ;
+        int flag = false;
+        int end = nums.size()-1;
+        int n = nums.size();
 
+        while(st<=end){
+            int mid = st+(end-st)/2;
+
+            if(nums[mid]==target || target<nums[mid]){
+
+                end = mid-1;
+            }
+            else if(target>nums[mid]){
+                st =mid+1;
+
+            }
+
+
+        }
+
+
+
+        return st;
     }
 };
