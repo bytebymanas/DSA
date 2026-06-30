2fce3ae0254fd0798a4d621d5bed3b124f678430
Manas Chhabra <163338402+bytebymanas@users.noreply.github.com>
2026-06-30 13:25:29 +0530

    Add gfg:Implement Lower Bound

---
+++ CODE/gfg/implement-lower-bound.js
@@ -0,0 +1,17 @@
+/**
+ * Problem: Implement Lower Bound
+ * Link: https://www.geeksforgeeks.org/problems/implement-lower-bound/1
+ * Difficulty: Unknown
+ * Platform: Gfg
+ * Language: JavaScript
+ * Tags: N/A
+ * Status: Accepted
+ */
+
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+        // code here
+
+    }
+};
